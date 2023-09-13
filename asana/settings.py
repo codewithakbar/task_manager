dfa7aa502412bc0e0a6b541bd4f92b05d2719e1d
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-09-13 10:32:13 +0500

    fadsfa

--- asana/settings.py
+++ asana/settings.py
@@ -74,14 +74,7 @@ SIMPLE_JWT = {
 }
 
 
-# CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-
-    "http://localhost:5173",
-    "http://127.0.0.1:9000",
-]
-
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_HEADERS = (
     "accept",
