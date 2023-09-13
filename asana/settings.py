ee11b09dd1a296949d02672dfc623570fe0b96fe
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-09-13 10:01:42 +0500

    SIMPLE_JWT

--- asana/settings.py
+++ asana/settings.py
@@ -68,8 +68,10 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True
 }
 
 
