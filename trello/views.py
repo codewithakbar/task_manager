e4441c02910b4eb6ad0a788d09c837d363e75bb2
AkbarAli <88106359+codewithakbar@users.noreply.github.com>
2023-09-25 00:48:41 +0500

    asdfdsaf

--- trello/views.py
+++ trello/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions, status
 from .models import Board, List, Card
 from .serializers import BoardSerializer, ListSerializer, CardSerializer
 
@@ -6,12 +7,16 @@ from rest_framework.authentication import SessionAuthentication
 
 
 class BoardViewSet(viewsets.ModelViewSet):
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    # permission_classes = (permissions.IsAuthenticated,)
     # authentication_classes = (SessionAuthentication,)
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
