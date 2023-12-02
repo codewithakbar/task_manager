07e600a6ff8ab5c8f21cd12f4347377ada2d0d2f
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-12-02 12:33:06 +0500

    asdf

--- trello/permissions.py
+++ trello/permissions.py
@@ -14,7 +14,7 @@ class IsOddiyAdminUser(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_staff:
             return bool(request.user and request.user.is_staff)
-        elif request.user.oddiy_admin:
+        if request.user.oddiy_admin:
             return bool(request.user and request.user.oddiy_admin)
 
 
