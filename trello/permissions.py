9c0df915c1e0611aa677abcc398888c3db5ec0ab
Satipov Akbar <88106359+codewithakbar@users.noreply.github.com>
2023-12-02 12:29:12 +0500

    fuck logic

--- trello/permissions.py
+++ trello/permissions.py
@@ -12,7 +12,11 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
 class IsOddiyAdminUser(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.oddiy_admin)
+        if request.user is request.user.is_staff:
+            return bool(request.user and request.user.is_staff)
+        elif request.user.oddiy_admin == True:
+            return bool(request.user and request.user.oddiy_admin)
+
 
 
 class IsAdminUser(permissions.BasePermission):
